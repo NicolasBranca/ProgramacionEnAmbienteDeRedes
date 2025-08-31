596fa5559cff9cd24c2ecada14a30ea0db26b458
NicolasBranca <nicolasfer1005@gmail.com>
2025-08-31 16:25:23 -0300

    correccion ej alcance

--- Jscript/js03_5_alcance/script.js
+++ Jscript/js03_5_alcance/script.js
@@ -1,28 +1,30 @@
 function asignacionConDeclaracion() {
     var valor = prompt("Ingrese un valor para la variable global (declaración + asignación):");
     var variableGlobal = valor;
-    document.getElementById("resultado").innerHTML =
-        "El valor de la variable local ingresada es: " + variableGlobal + "<br>" +
-        "El tipo de la variable local ingresada es: " + typeof variableGlobal;
+    
+    alert(
+        "El valor de la variable local ingresada es: " + variableGlobal + "\n" +
+        "El tipo de la variable local ingresada es: " + typeof variableGlobal
+    );
 }
 
 function asignacionSinDeclaracion() {
     variableGlobal = prompt("Ingrese un valor para la variable global (asignación forzada):");
-    document.getElementById("resultado").innerHTML =
-        "El valor de la variable global ingresada es: " + variableGlobal + "<br>" +
-        "El tipo de la variable global ingresada es: " + typeof variableGlobal;
+
+    alert(
+        "El valor de la variable global ingresada es: " + variableGlobal + "\n" +
+        "El tipo de la variable global ingresada es: " + typeof variableGlobal
+    );
 }
 
 function mostrarVariableGlobal() {
-    const div = document.getElementById("resultado");
     if (typeof variableGlobal === "undefined" || variableGlobal === "") {
         alert("La variable global no ha sido asignada");
-        div.innerHTML = "";
     } else {
-        alert("El valor de la variableGlobal es: " + variableGlobal);
-        div.innerHTML =
-            "El valor de la variableGlobal es: " + variableGlobal + "<br>" +
-            "La variableGlobal es de tipo: " + typeof variableGlobal;
+        alert(
+            "El valor de la variableGlobal es: " + variableGlobal + "\n" +
+            "La variableGlobal es de tipo: " + typeof variableGlobal
+        );
     }
 }
 
