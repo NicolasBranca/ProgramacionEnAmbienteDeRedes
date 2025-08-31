de1f03e7149b6fd839d1105772a48db344f01692
NicolasBranca <nicolasfer1005@gmail.com>
2025-08-31 12:35:24 -0300

    js02 comando en evento

---
+++ Jscript/js02_comando_en_evento/script.js
@@ -0,0 +1,7 @@
+function alertOnClick() {
+    alert("Sentencia Alert() ejecutada en evento OnClick del botón pulsado.");
+}
+
+function writeOnClick() {
+    document.write("hola. Esta escritura pisa lo anterior");
+}
