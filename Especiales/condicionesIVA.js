fc08d60b1dbe309243dd052dbf7ce7ee66434876
NicolasBranca <nicolasfer1005@gmail.com>
2025-09-21 17:00:33 -0300

    ejercicio 05 especiales

---
+++ Especiales/condicionesIVA.js
@@ -0,0 +1,8 @@
+const condicionesIVAJSON = {
+  condicionesIVA: [
+    { idIVA: 1, tipoIVA: "General", porcentaje: 21 },
+    { idIVA: 2, tipoIVA: "Reducido", porcentaje: 10.5 },
+    { idIVA: 3, tipoIVA: "Aumentado", porcentaje: 27 },
+    { idIVA: 4, tipoIVA: "Exento", porcentaje: 0 }
+  ]
+};
